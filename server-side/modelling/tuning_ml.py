c262f70fe8c5f7ffcbe87731a489e7449fbf6ab8
08Aristodemus24 <MichaelAveuc571@gmail.com>
2024-10-17 13:22:02 +0800

    update

--- server-side/modelling/tuning_ml.py
+++ server-side/modelling/tuning_ml.py
@@ -529,7 +529,7 @@ if __name__ == "__main__":
         'svm': {
             # used in Taylor et al. (2015) and Hossain et al. (2022)
             'model': SVC, 
-            'hyper_params': {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.01, 0.1, 1], 'probability': [True]}
+            'hyper_params': {'kernel': ['rbf'], 'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.01, 0.1, 1], 'probability': [True]}
             # 'model': LinearSVC, 
             # 'hyper_params': {'C': [1, 10, 100, 1000]}
         },
